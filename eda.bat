82e5b71fb0453814642c6017bbb7b847a878c1ef
edloidas <edloidas@gmail.com>
2018-01-31 12:22:39 +0100

    Update windows script code style

--- eda.bat
+++ eda.bat
@@ -1,214 +1,216 @@
-@ECHO OFF
+@echo off
 
-REM =========
-REM User variables. Should be defined before first launch
-REM =========
-SET XP_REPO=c:\repo
-SET XP_HOME=c:\tmp\xp_home
+:: ==========================
+:: VARIABLES
+:: ==========================
+:: - System -----------------
+set pathRepo=c:\repo
+set pathHome=c:\tmp\xp_home
+:: - Repos ------------------
+set pathXp=c:\repo\xp
+set pathLib=c:\repo\lib-admin-ui
+set pathApps=c:\repo\xp-apps
+:: - Runtime ----------------
+:: Run:   runDebug, runDev
+:: Build: buildRerun, buildAll, buildSkipTest, buildSkipLint
+:: Tasks: taskBuild, taskClean, taskLint, taskTest, taskRun, taskInit
+:: Only:  onlyXp, onlyLib, onlyApps
+:: Group: groupDefault, groupQuick, groupFull
+set runDev=dev
 
-REM =========
-REM Runtime variables
-REM =========
+:: ==========================
+:: RUNNERS
+:: ==========================
+if "%1"=="" set groupDefault=1
+call:parseArguments %*
 
-REM   Task "run":
-SET ARG_DEBUG=""
-SET ARG_DEV="dev"
+if not "%help%"=="" call:helpFunc
+if not "%groupDefault%"=="" call:defaultFunc
+if not "%groupQuick%"=="" call:quickFunc
+if not "%groupFull%"=="" call:fullFunc
 
-REM   Task "build":
-SET ARG_RERUN=""
-SET ARG_DEBUG=""
-SET ARG_DEV=""
-SET ARG_SKIP_LINT=""
-SET ARG_SKIP_TEST=""
-SET ARG_ALL=""
+if not "%taskInit%"=="" call:initFunc
+if not "%taskLint%"=="" call:lintFunc
+if not "%taskClean%"=="" (if "%taskBuild%"=="" call:cleanFunc)
+if not "%taskBuild%"=="" call:buildFunc
+if not "%taskRun%"=="" call:runFunc
 
-REM   Tasks flags:
-REM     DO_BUILD, DO_LINT, DO_TEST, DO_RUN, DO_INIT, DO_DEFAULT
-REM     DO_BUILD_ONLY_XP, DO_BUILD_ONLY_LIB, DO_BUILD_ONLY_APPS
+:: ==========================
+:: FUNCTIONS
+:: ==========================
+exit 0
+:: - Parse arguments --------
+:parseArguments
+  if not "%1"=="" (
+    if "%1"=="--help"  ( set help=1&                   shift & goto:eof )
+    if "%1"=="-h"      ( set help=1&                   shift & goto:eof )
 
-REM =========
-REM Arguments parsing
-REM =========
-IF "%~1" == "" (
-  SET DO_DEFAULT=1
-)
+    if "%1"=="build"   ( set taskBuild=1&              shift & goto :parseArguments )
+    if "%1"=="clean"   ( set taskClean=clean&          shift & goto :parseArguments )
+    if "%1"=="lint"    ( set taskLint=1&               shift & goto :parseArguments )
+    if "%1"=="test"    ( set taskTest=1&               shift & goto :parseArguments )
+    if "%1"=="run"     ( set taskRun=1&                shift & goto :parseArguments )
+    if "%1"=="init"    ( set taskInit=1&               shift & goto :parseArguments )
+    if "%1"=="default" ( set groupDefault=1&           shift & goto :parseArguments )
+    if "%1"=="quick"   ( set groupQuick=1&             shift & goto :parseArguments )
+    if "%1"=="full"    ( set groupFull=1&              shift & goto :parseArguments )
+    if "%1"=="-r"      ( set buildRerun=--rerun-tasks& shift & goto :parseArguments )
+    if "%1"=="-d"      ( set runDebug=debug&           shift & goto :parseArguments )
+    if "%1"=="-p"      ( set runDev=&                  shift & goto :parseArguments )
+    if "%1"=="-a"      ( set buildAll=-Pall&           shift & goto :parseArguments )
+    if "%1"=="-x" (
+      :skip
+      if "%2"=="lint"  ( set buildSkipLint=-x lint&    shift & goto :skip )
+      if "%2"=="test"  ( set buildSkipTest=-x test&    shift & goto :skip )
+      shift & goto :parseArguments )
+    if "%1"=="-o" (
+      :only
+      if "%2"=="xp"           ( set onlyXp=1&          shift & goto :only )
+      if "%2"=="lib-admin-ui" ( set onlyLib=1&         shift & goto :only )
+      if "%2"=="xp-apps"      ( set onlyApps=1&        shift & goto :only )
+      shift & goto :parseArguments )
+    shift & goto :parseArguments )
 
-:loop
-IF NOT "%1"=="" (
-  IF "%1"=="build" (
-    SET DO_BUILD=1
-    SHIFT & GOTO :loop
+  :: Update flags with complicated state
+  if "%onlyXp%"=="" (if "%onlyLib%"=="" (if "%onlyApps%"=="" (
+    set   onlyXp=1
+    set  onlyLib=1
+    set onlyApps=1
+  )))
+
+  if not [%taskLint%]==[] (
+    if not "%buildSkipLint%"=="" set taskLint=
+    if not "%taskBuild%"==""     set taskLint=
   )
-  IF "%1"=="clean" (
-    SET DO_CLEAN=1
-    SHIFT & GOTO :loop
-  )
-  IF "%1"=="lint" (
-    SET DO_LINT=1
-    SHIFT & GOTO :loop
-  )
-  IF "%1"=="test" (
-    SET DO_TEST=1
-    SHIFT & GOTO :loop
-  )
-  IF "%1"=="run" (
-    SET DO_RUN=1
-    SHIFT & GOTO :loop
-  )
-  IF "%1"=="init" (
-    SET DO_INIT=1
-    SHIFT & GOTO :loop
-  )
-  IF "%1"=="default" (
-    SET DO_DEFAULT=1
-    SHIFT & GOTO :loop
-  )
-  IF "%1"=="-r" (
-    SET ARG_RERUN="--rerun-tasks"
-    SHIFT & GOTO :loop
-  )
-  IF "%1"=="-d" (
-    SET ARG_DEBUG="debug"
-    SHIFT & GOTO :loop
-  )
-  IF "%1"=="-p" (
-    SET ARG_DEV=""
-    SHIFT & GOTO :loop
-  )
-  IF "%1"=="-a" (
-    SET ARG_ALL="-Pall"
-    SHIFT & GOTO :loop
-  )
-  IF "%1"=="-x" (
-    :skip
-    IF "%2"=="lint" (
-      SET ARG_SKIP_LINT="-x lint"
-      SHIFT & GOTO :skip
-    )
-    IF "%2"=="test" (
-      SET ARG_SKIP_TEST="-x test"
-      SHIFT & GOTO :skip
-    )
-    SHIFT & GOTO :loop
-  )
-  IF "%1"=="-o" (
-    :only
-    IF "%2"=="xp" (
-      SET DO_BUILD_ONLY_XP=1
-      SHIFT & GOTO :only
-    )
-    IF "%2"=="lib-admin-ui" (
-      SET DO_BUILD_ONLY_LIB=1
-      SHIFT & GOTO :only
-    )
-    IF "%2"=="xp-apps" (
-      SET DO_BUILD_ONLY_APPS=1
-      SHIFT & GOTO :only
-    )
-    SHIFT & GOTO :loop
-  )
-  IF "%1"=="--help" (
-    SET HELP=1
-    SHIFT & GOTO :loopend
-  )
-  SHIFT & GOTO :loop
-)
-:loopend
 
-REM =========
-REM Executing tasks
-REM =========
-
-REM   --help
-IF NOT [%HELP%]==[] (
-  ECHO Usage: & ECHO. & ECHO   eda [..TASKS] [..FLAGS]
-  ECHO. & ECHO Tasks and flags:
-  ECHO. & ECHO   init         # Clone repos [xp, lib-admin-ui, xp-apps]
-  ECHO. & ECHO   clean        # Clean builded files of REPO [xp, lib-admin-ui, xp-apps]
-  ECHO     -o [REPO]  # Build only a specific REPO [xp, lib-admin-ui, xp-apps]
-  ECHO. & ECHO   build        # Build all REPO [xp, lib-admin-ui, xp-apps]
-  ECHO     -r         # Rerun cached Gradle tasks [--rerun-tasks]
-  ECHO     -a         # Use local lib-admin-ui repo, when building xp-apps [-Pall]
-  ECHO     -x [TASK]  # Skip Gradle tasks, while building [test, lint]
-  ECHO     -o [REPO]  # Build only a specific REPO [xp, lib-admin-ui, xp-apps]
-  ECHO. & ECHO   run          # Run server at the end
-  ECHO     -d         # Run server in "debug" mode with open port :8080
-  ECHO     -p         # Run server in production [without "dev"] and HMR
-  ECHO. & ECHO   default      # Only task, executes for [xp-apps] and [lib-admin-ui]
-  ECHO                # gradle build deploy -x lint -x test -Pall
-  ECHO. & ECHO Examples:
-  ECHO   eda build -o xp run -d
-  ECHO   eda clean build -x test -o xp-apps -a
-  EXIT 0
-)
-
-REM   Default task, with no args or with "default" argument
-IF NOT [%DO_DEFAULT%]==[] (
-  ECHO. & ECHO Building LIB-ADMIN-UI: & ECHO.
-  gradle build -x lint -x test -p %XP_REPO%\lib-admin-ui
-
-  ECHO. & ECHO Building XP-APPS:
-  gradle build deploy -x lint -x test -Pall -p %XP_REPO%\xp-apps
-
-  ECHO. & ECHO %TIME% & ECHO.
-
-  EXIT 0
-)
-
-REM   Initializing with git clone
-IF NOT [%DO_INIT%]==[] (
-  ECHO Initializing...
-  git clone https://github.com/enonic/xp.git %XP_REPO%\xp
-  git clone https://github.com/enonic/lib-admin-ui.git %XP_REPO%\lib-admin-ui
-  git clone https://github.com/enonic/xp-apps.git %XP_REPO%\xp-apps
-)
-
-REM   Task "clean"
-IF NOT [%DO_CLEAN%]==[] (
-  ECHO Cleaning...
-  IF NOT [%DO_BUILD_ONLY_XP%]==[] ( gradle clean -p %XP_REPO%\xp )
-  IF NOT [%DO_BUILD_ONLY_LIB%]==[] ( gradle clean -p %XP_REPO%\lib-admin-ui )
-  IF NOT [%DO_BUILD_ONLY_APPS%]==[] ( gradle clean -p %XP_REPO%\xp-apps )
-  IF [%DO_BUILD_ONLY_XP%]==[] ( IF [%DO_BUILD_ONLY_LIB%]==[] ( IF [%DO_BUILD_ONLY_APPS%]==[] (
-    gradle clean -p %XP_REPO%\xp
-    gradle clean -p %XP_REPO%\lib-admin-ui
-    gradle clean -p %XP_REPO%\xp-apps
-  ) ) )
-)
-
-REM   Task "build"
-IF NOT [%DO_BUILD%]==[] (
-  ECHO Building...
-  IF NOT [%DO_BUILD_ONLY_XP%]==[] (
-    ECHO. & ECHO Building XP: %ARG_SKIP_TEST:"=% %ARG_RERUN:"=%
-    gradle build %ARG_SKIP_TEST:"=% %ARG_RERUN:"=% -p %XP_REPO%\xp
+  if not "%taskTest%"=="" (
+    if not "%buildSkipTest%"=="" set taskTest=
+    if not "%taskBuild%"==""     set taskTest=
   )
-  IF NOT [%DO_BUILD_ONLY_LIB%]==[] (
-    ECHO. & ECHO Building LIB-ADMIN-UI: %ARG_SKIP_LINT:"=% %ARG_SKIP_TEST:"=% %ARG_RERUN:"=%
-    gradle build %ARG_SKIP_LINT:"=% %ARG_SKIP_TEST:"=% %ARG_RERUN:"=% -p %XP_REPO%\lib-admin-ui
-  )
-  IF NOT [%DO_BUILD_ONLY_APPS%]==[] (
-    IF [%DO_BUILD_ONLY_LIB%]==[] ( IF %ARG_ALL%=="-Pall" (
-        ECHO. & ECHO Building LIB-ADMIN-UI: %ARG_SKIP_LINT:"=% %ARG_SKIP_TEST:"=% %ARG_RERUN:"=%
-        gradle build %ARG_SKIP_LINT:"=% %ARG_SKIP_TEST:"=% %ARG_RERUN:"=% -p %XP_REPO%\lib-admin-ui
-    ) )
-    ECHO. & ECHO Building XP-APPS: %ARG_SKIP_LINT:"=% %ARG_SKIP_TEST:"=% %ARG_RERUN:"=% %ARG_ALL:"=%
-    gradle build deploy %ARG_SKIP_LINT:"=% %ARG_SKIP_TEST:"=% %ARG_RERUN:"=% %ARG_ALL:"=% -p %XP_REPO%\xp-apps
-  )
-  IF [%DO_BUILD_ONLY_XP%]==[] ( IF [%DO_BUILD_ONLY_LIB%]==[] ( IF [%DO_BUILD_ONLY_APPS%]==[] (
-        ECHO. & ECHO Building XP: %ARG_SKIP_TEST:"=% %ARG_RERUN:"=%
-        gradle build %ARG_SKIP_TEST:"=% %ARG_RERUN:"=% -p %XP_REPO%\xp
-        ECHO. & ECHO Building LIB-ADMIN-UI: %ARG_SKIP_LINT:"=% %ARG_SKIP_TEST:"=% %ARG_RERUN:"=%
-        gradle build %ARG_SKIP_LINT:"=% %ARG_SKIP_TEST:"=% %ARG_RERUN:"=% -p %XP_REPO%\lib-admin-ui
-        ECHO. & ECHO Building XP-APPS: %ARG_SKIP_LINT:"=% %ARG_SKIP_TEST:"=% %ARG_RERUN:"=% %ARG_ALL:"=%
-        gradle build deploy %ARG_SKIP_LINT:"=% %ARG_SKIP_TEST:"=% %ARG_RERUN:"=% %ARG_ALL:"=% -p %XP_REPO%\xp-apps
-  ) ) )
 
-  ECHO. & ECHO %TIME% & ECHO.
-)
+  if not "%taskInit%"=="" set taskClean=
 
-REM   Task "run"
-IF NOT [%DO_RUN%]==[] (
-  ECHO. & ECHO Starting server: %ARG_DEBUG:"=% %ARG_DEV:"=%
-  CALL %XP_REPO%\xp\modules\runtime\build\install\bin\server.bat %ARG_DEBUG:"=% %ARG_DEV:"=% -Dxp.home=%XP_HOME%
-)
+  if not "%groupDefault%"=="" (if not "%groupQuick%"=="" echo `default` and `quick` tasks cann't run simultaneously & exit 0)
+  if not "%groupDefault%"=="" (if not "%groupFull%"==""  echo `default` and `full` tasks cann't run simultaneously & exit 0)
+  if not "%groupFull%"==""    (if not "%groupQuick%"=="" echo `full` and `quick` tasks cann't run simultaneously & exit 0)
+goto:eof
+
+:: - Help & manual ----------
+:helpFunc
+  echo.
+  echo Usage: & echo.
+  echo   eda [..TASKS] [..FLAGS] & echo.
+  echo Tasks and flags: & echo.
+  echo   init         # Clone repositories. & echo.
+  echo   clean        # Clean builded files in repositories. & echo.
+  echo   lint         # Lint sources in repositories. & echo.
+  echo   build        # Build all repositories [xp, lib-admin-ui, xp-apps].
+  echo     -r         # Rerun cached Gradle tasks [--rerun-tasks].
+  echo     -a         # Use local lib-admin-ui repo, when building xp-apps [-Pall]. & echo.
+  echo   -x [TASK]    # Skip specific Grale task [test, lint]. & echo.
+  echo   -o [REPO]    # Run task for the specific repository [xp, lib-admin-ui, xp-apps].
+  echo   run          # Run server.
+  echo     -d         # Enable "debug" mode with open port :5005
+  echo     -p         # Enable production environment. HMR will be disabled. & echo.
+  echo Grouped tasks: & echo.
+  echo   default      # Build local lib-admin-ui and xp-apps, without test and lint. & echo.
+  echo   quick        # Build local xp-apps, without test and lint. & echo.
+  echo   full         # Build all repositories, without test and lint. & echo.
+  echo Examples:
+  echo   eda build -o xp run -d
+  echo   eda clean build -x test lint -o xp xp-apps -a
+  echo.
+exit 0
+
+:: - Git clone --------------
+:initFunc
+`echo. & echo = Clone =
+  if not "%onlyXp%"=="" git clone https://github.com/enonic/xp.git %pathXp%
+  if not "%onlyLib%"=="" git clone https://github.com/enonic/lib-admin-ui.git %pathLib%
+  if not "%onlyApps%"=="" git clone https://github.com/enonic/xp-apps.git %pathApps%
+goto:eof
+
+:: - Linting ----------------
+:lintFunc
+  echo. & echo = Lint =
+  if not "%onlyLib%"=="" gradle lint -p %pathLib%
+  if not "%onlyApps%"=="" gradle lint -p %pathApps%
+goto:eof
+
+:: - Testing ----------------
+:lintFunc
+  echo. & echo = Test =
+  if not "%onlyXp%"=="" gradle test -p %pathXp%
+  if not "%onlyLib%"=="" gradle test -p %pathLib%
+  if not "%onlyApps%"=="" gradle test -p %pathApps%
+goto:eof
+
+:: - Cleaning ---------------
+:cleanFunc
+  echo. & echo = Clean =
+  if not "%onlyXp%"=="" gradle clean -p %pathXp%
+  if not "%onlyLib%"=="" gradle clean -p %pathLib%
+  if not "%onlyApps%"=="" gradle clean -p %pathApps%
+goto:eof
+
+:: - Building ---------------
+:buildFunc
+  echo. & echo = Build =
+  if not "%onlyXp%"=="" call:buildXpFunc
+  if not "%onlyLib%"=="" call:buildLibFunc
+  if not "%onlyApps%"=="" call:buildAppsFunc
+  echo. & echo %TIME% & echo.
+goto:eof
+
+:buildXpFunc
+  echo. & echo = XP =
+  echo gradle %taskClean% build %buildSkipTest% %buildRerun% -p %pathXp%
+  call gradle %taskClean% build %buildSkipTest% %buildRerun% -p %pathXp%
+goto:eof
+
+:buildLibFunc
+  echo. & echo = LIB ADMIN UI =
+  echo gradle %taskClean% build %buildSkipLint% %buildSkipTest% %buildRerun% -p %pathLib%
+  call gradle %taskClean% build %buildSkipLint% %buildSkipTest% %buildRerun% -p %pathLib%
+goto:eof
+
+:buildAppsFunc
+  echo. & echo = XP APPS =
+  echo gradle %taskClean% build deploy %buildAll% %buildSkipLint% %buildSkipTest% %buildRerun% -p %pathApps%
+  call gradle %taskClean% build deploy %buildAll% %buildSkipLint% %buildSkipTest% %buildRerun% -p %pathApps%
+goto:eof
+
+:: - Running server ---------
+:runFunc
+  echo. & echo = Run Server =
+  if not "%runDev%"==""   echo DEV: ON
+  if not "%runDebug%"=="" echo DEBUG: ON & echo.
+  call %pathXp%\modules\runtime\build\install\bin\server.bat %runDebug% %runDev% -Dxp.home=%pathHome%
+exit 0
+
+:: - Default ----------------
+:defaultFunc
+  echo. & echo = Default =
+  echo. & echo gradle build -x lint -x test -p %pathLib%
+  call gradle build -x lint -x test -p %pathLib%
+  echo. & echo gradle build deploy -Pall -x lint -x test -p %pathApps%
+  call gradle build deploy -Pall -x lint -x test -p %pathApps%
+exit 0
+
+:: - Quick ------------------
+:quickFunc
+  echo. & echo = Quick =
+  echo. & echo gradle build deploy -x lint -x test -p %pathApps%
+  call gradle build deploy -x lint -x test -p %pathApps%
+exit 0
+
+:: - Full ------------------
+:fullFunc
+  echo. & echo = Full =
+  echo. & echo gradle build -x test -p %pathXp%
+  call gradle build -x test -p %pathXp%
+  echo. & echo gradle build -x lint -x test -p %pathLib%
+  call gradle build -x lint -x test -p %pathLib%
+  echo. & echo gradle build deploy -Pall -x lint -x test -p %pathApps%
+  call gradle build deploy -Pall -x lint -x test -p %pathApps%
+exit 0
